e941f549a23ed7eb1a39aea9e259ebfff7a5e903
Paquitosoft <paquitosoftware@gmail.com>
2019-11-24 23:45:21 +0100

    First version of season detail view

--- src/components/season-detail/season-detail-view.js
+++ src/components/season-detail/season-detail-view.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SEASON_DETAIL_MOCK from '../../fixtures/season-detail.json';
 import TVSHOW_DETAIL_MOCK from '../../fixtures/tvshow-detail.json';
+
+import './season-detail-view.css';
 
 function EpisodeCard({ episode }) {
 	const {
@@ -12,23 +14,37 @@ function EpisodeCard({ episode }) {
 		voteAverage,
 		overview
 	} = episode;
+	const [isOverviewVisible, toggleOverviewVisibility] = useState(false);
 
 	return (
 		<div className="episode-card">
 			<img src={`https://image.tmdb.org/t/p/w454_and_h254_bestv2${imagePath}`} alt={name} className="episode-card__media"/>
 			<div className="episode-card__info">
 				<div className="episode-card__row">
-					<span className="episode-card__number">{episodeNumber}</span>
-					<span className="episode-card__title">{name}</span>
+					<div className="episode-card__title">
+						<span className="episode-card__number">{episodeNumber}.&nbsp;</span>
+						<span className="episode-card__name">{name}</span>
+					</div>
+					<span className="episode-card__rating">{voteAverage.toFixed(1)}</span>
 				</div>
 				<div className="episode-card__row">
-					<span className="episode-card__date">{airDate}</span>
-					<span className="episode-card__rating">{voteAverage}</span>
-				</div>
-				<div className="episode-card__row">
-					<p className="episode-card__overview">{overview}</p>
+					<span className="episode-card__date">({airDate})</span>
+					<span 
+						className="episode-card__overview-link"
+						onClick={() => toggleOverviewVisibility(true)}
+					>See overview</span>
 				</div>
 			</div>
+			{isOverviewVisible && 
+				<div 
+					className="episode-card__overview-panel"
+					onClick={() => toggleOverviewVisibility(false)}
+				>
+					<div className="episode-card__overview-content">
+						{overview}
+					</div>
+				</div>
+			}
 		</div>
 	);
 }
@@ -42,10 +58,10 @@ export default function SeasonDetailView({ season = SEASON_DETAIL_MOCK, tvShow =
 
 	return (
 		<section className="season-detail">
-			<header>
+			<header className="season-detail__header">
 				<h1 className="season-detail__season-name">{name}</h1>
-				<h2 className="season-detail__season-date">({airDate})</h2>
-				<h2 className="season-detail__tvshow-name">{tvShow.name}</h2>
+				<span className="season-detail__season-date">({airDate})</span>
+				<span className="season-detail__tvshow-name"><i>{tvShow.name}</i></span>
 			</header>
 			<section className="season-detail__episodes">
 				{episodes.map(episode => 
