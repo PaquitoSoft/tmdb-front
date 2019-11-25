e2e8f1f507e23b1f2337e89a9d816a5bf405f3d3
PaquitoSoft <paquitosoftware@gmail.com>
2019-11-25 22:56:41 +0100

    Updates

---
+++ precache-manifest.6cf3f61fbcfa70b9b8e385575d2f6c75.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75bc7bf484c8229e0369dd880189c65",
+    "url": "/tmdb-front/index.html"
+  },
+  {
+    "revision": "5387b0ed00e223675e63",
+    "url": "/tmdb-front/static/css/3.e7250191.chunk.css"
+  },
+  {
+    "revision": "f230272bfe4a9b9ef74e",
+    "url": "/tmdb-front/static/css/4.40224ba2.chunk.css"
+  },
+  {
+    "revision": "9d0d963f223d85b6d7c8",
+    "url": "/tmdb-front/static/css/5.d6bc2cf6.chunk.css"
+  },
+  {
+    "revision": "64e2c4ad2d8b6d1aa41f",
+    "url": "/tmdb-front/static/css/main.764d771e.chunk.css"
+  },
+  {
+    "revision": "62e72abf7525154533da",
+    "url": "/tmdb-front/static/js/2.d46c170c.chunk.js"
+  },
+  {
+    "revision": "5387b0ed00e223675e63",
+    "url": "/tmdb-front/static/js/3.e627f207.chunk.js"
+  },
+  {
+    "revision": "f230272bfe4a9b9ef74e",
+    "url": "/tmdb-front/static/js/4.ac13278b.chunk.js"
+  },
+  {
+    "revision": "9d0d963f223d85b6d7c8",
+    "url": "/tmdb-front/static/js/5.cedb58dc.chunk.js"
+  },
+  {
+    "revision": "64e2c4ad2d8b6d1aa41f",
+    "url": "/tmdb-front/static/js/main.7d00d801.chunk.js"
+  },
+  {
+    "revision": "523c03ff37c56268df58",
+    "url": "/tmdb-front/static/js/runtime-main.f0b1f544.js"
+  }
+]);
